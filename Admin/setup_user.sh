25c4f7364ffcb4924b445edd7271072eb41ff04e
Kye Morton <pryre.dev@outlook.com>
2018-09-07 14:02:05 +1000

    Uncommented testing commands

--- Admin/setup_user.sh
+++ Admin/setup_user.sh
@@ -13,11 +13,11 @@ set -e
 
 echo "Setting up user account: $NEW_USER"
 
-#sudo useradd -m $NEW_USER -p $NEW_USER
+sudo useradd -m $NEW_USER -p $NEW_USER
 sudo usermod -aG $(cat groups_user.txt) $NEW_USER
-#/usr/share/ros-workstation/copy-config2user.sh $NEW_USER
-#sudo chsh -s /bin/bash $NEW_USER
-#sudo su $NEW_USER -c /usr/share/ros-workstation/setup-catkin-workspace.sh
+/usr/share/ros-workstation/copy-config2user.sh $NEW_USER
+sudo chsh -s /bin/bash $NEW_USER
+sudo su $NEW_USER -c /usr/share/ros-workstation/setup-catkin-workspace.sh
 sudo su $NEW_USER -c /usr/share/ros-workstation/setup-catkin-workspace.sh
 cp /usr/share/ros-workstation/QUT\ Internet\ Login.desktop /usr/share/ros-workstation/QGroundControl.desktop /home/$NEW_USER/Desktop
 sudo chown $NEW_USER /home/$NEW_USER/Desktop/*
