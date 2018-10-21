6b350bc75aa752d81bf68a0eb51cb237f030b9d5
pryre <pryre@users.noreply.github.com>
2018-10-22 08:07:11 +1000

    Update image_saver.py

--- Scripts/image_saver.py
+++ Scripts/image_saver.py
@@ -1,34 +1,76 @@
 #!/usr/bin/env python
 
-IMAGE_TOPIC = '/webcam/image_raw/compressed'
-
+import sys
 import os
 import rospy
+import rostopic
 from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 
-def callback(msg_in):
-	ts = str(msg_in.header.stamp.secs) + '-' + str(msg_in.header.stamp.nsecs)
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 
-	fmt = ""
-	if "png" in msg_in.format:
-		fmt = "png"
-	elif ("jpeg" in msg_in.format) or ("jpg" in msg_in.format):
-		fmt = "jpg"
-	else:
-		rospy.logwarn("Ignoring image, unkown format: %s" % str(msg_in.format) )
+global bridge
+global use_compressed
 
-	if fmt:
-		filename = 'image_' + ts + '.' + fmt
+def callback_image(msg_in):
+	global bridge
+	global use_compressed
 
-		rospy.logdebug("saving: " + filename)
-
-		f = open(filename, 'w')
-		f.write(msg_in.data)
-		f.close()
+	try:
+		if use_compressed:
+			cv_image = bridge.compressed_imgmsg_to_cv2( msg_in, "bgr8" )
+		else:
+			cv_image = bridge.imgmsg_to_cv2( msg_in, "bgr8" )
+			
+		ts = str(msg_in.header.stamp.secs) + '-' + str(msg_in.header.stamp.nsecs)
+		filename = "image_" + ts + ".png"
+		rospy.logdebug("saving: %s" % filename)
+		
+		cv2.imwrite(filename, cv_image)
+	except CvBridgeError as e:
+		rospy.loginfo(e)
+		return
 
 if __name__ == '__main__':
-  rospy.init_node('saver_py')
-  rospy.Subscriber(IMAGE_TOPIC, CompressedImage, callback)
+	global bridge
+	global use_compressed
+	success = False
+	img_sub = None
+	img_topic = ""
 
-  rospy.loginfo("Started image saver node")
-  rospy.spin()
+	rospy.init_node('iamge_saver_py', anonymous=True)
+	
+	if len(sys.argv) == 2:
+		img_topic = sys.argv[1]
+	else:
+		rospy.logerr("Unable to parse input topic!")
+		rospy.logerr("\nUsage:\n\tpython image_saver.py /camera_name/image_topic\n\tpython image_saver.py /camera_name/image_topic_compressed")
+		rospy.signal_shutdown("bad args")
+
+	if not rospy.is_shutdown():
+		data_type = rostopic.get_topic_type(img_topic, blocking=True)[0]
+	
+		if data_type == "sensor_msgs/Image":
+			img_sub = rospy.Subscriber(img_topic, Image, callback_image)
+			use_compressed = False
+			success = True
+		elif data_type == "sensor_msgs/CompressedImage":
+			img_sub = rospy.Subscriber(img_topic, CompressedImage, callback_image)
+			
+			use_compressed = True
+			success = True
+		else:
+			rospy.logerr("Unsupported message type (%s)" % data_type)
+
+		if success:
+			try:				
+				bridge = CvBridge()
+			
+				rospy.loginfo("Started image saver node")
+				rospy.spin()
+			except:
+				pass
+			finally:
+				if img_sub is not None:
+					img_sub.unregister()
