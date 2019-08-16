14317a4a071f59c3a47e6f55ceb5a1ae9df72e2f
Kye Morton <pryre.dev@outlook.com>
2019-08-16 15:54:49 +1000

    updates to user setup

--- Admin/setup_user.sh
+++ Admin/setup_user.sh
@@ -14,11 +14,13 @@ set -e
 
 echo "Setting up user account: $NEW_USER"
 
-sudo useradd -m $NEW_USER -p $NEW_USER
-sudo usermod -aG $(cat groups_user.txt) $NEW_USER
+sudo useradd -m $NEW_USER -s /bin/bash -G $(cat groups_user.txt)
+echo "$NEW_USER:$NEW_USER" | sudo chpasswd
 /usr/share/ros-workstation/copy-config2user.sh $NEW_USER
-sudo chsh -s /bin/bash $NEW_USER
+
 sudo su $NEW_USER -c /usr/share/ros-workstation/setup-catkin-workspace.sh
-sudo su $NEW_USER -c /usr/share/ros-workstation/setup-catkin-workspace.sh
+
+sudo mkdir -p /home/$NEW_USER/Desktop/
 sudo cp /usr/share/ros-workstation/QUT\ Internet\ Login.desktop /usr/share/ros-workstation/QGroundControl.desktop /home/$NEW_USER/Desktop
+sudo chown $NEW_USER /home/$NEW_USER/Desktop
 sudo chown $NEW_USER /home/$NEW_USER/Desktop/*
