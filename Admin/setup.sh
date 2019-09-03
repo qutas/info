26beff5e08152a7417bcbbf109626604989b0a6e
Kye Morton <pryre.dev@outlook.com>
2019-09-03 12:37:06 +1000

    Fixed some copying flags, etc.

--- Admin/setup.sh
+++ Admin/setup.sh
@@ -13,12 +13,13 @@ sudo usermod -aG $(cat ./groups_admin.txt) quas
 
 xfce4-panel --quit
 pkill xfconfd
-cp ./home/quas ~/
+cp -R ./home/quas/* ~/
 xfce4-panel &
 
 sudo mkdir /usr/share/ros-workstation #Create shared driver folder
-cp ./usr/share/ros-workstation /usr/share/ros-workstation
-mkdir -p /usr/share/ros-workstation/catkin_drv/src
 sudo chown -R quas:quas /usr/share/ros-workstation/	#Give write access to quas
 sudo chmod a+r /usr/share/ros-workstation/ #Give read access to all
+
+cp -r ./usr/share/ros-workstation/* /usr/share/ros-workstation
+mkdir -p /usr/share/ros-workstation/catkin_drv/src
 ln -s /usr/share/ros-workstation/ ~/
