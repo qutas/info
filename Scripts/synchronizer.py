ce17133d5df1268491f4cf0849ac7937418b359d
pryre <pryre@users.noreply.github.com>
2020-03-30 14:45:52 +1000

    Update synchronizer.py

--- Scripts/synchronizer.py
+++ Scripts/synchronizer.py
@@ -2,25 +2,28 @@
 
 import rospy
 import message_filters
-from std_msgs.msg import Int32
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import CompressedImage
 
-def callback(gas_a, gas_b):
-	rospy.loginfo("Synced Measurements: [%i, %i]", gas_a.data, gas_b.data)
+def callback(msg_in_pose, msg_in_image):
+	rospy.loginfo("Synced Measurements:")
+	rospy.loginfo(msg_in_pose)
+	rospy.loginfo(msg_in_image.format)
 
 if __name__ == '__main__':
 	# Initialize
-	rospy.init_node('filter_py')
+	rospy.init_node('synchronizer_py')
 
-	ga_sub = message_filters.Subscriber('/emulator/sensors/gas/a', Int32, queue_size=10)
-	gb_sub = message_filters.Subscriber('/emulator/sensors/gas/b', Int32, queue_size=10)
-	ts = message_filters.ApproximateTimeSynchronizer([ga_sub, gb_sub], 10, 0.1, allow_headerless=True)
+	sub_pose = message_filters.Subscriber('/emulated_uav/pose', PoseStamped, queue_size=10)
+	sub_image = message_filters.Subscriber('/emulator/iamge/compressed', CompressedImage, queue_size=10)
+	ts = message_filters.ApproximateTimeSynchronizer([sub_pose, sub_image], 10, 0.1, allow_headerless=True)
 	ts.registerCallback(callback)
 
 	# Loop here until quit
-		try:
-		rospy.loginfo("Started subscriber node...")
+	try:
+		rospy.loginfo("Started synchronizer node...")
 		rospy.spin()
 	except rospy.ROSInterruptException:
 		# Shutdown
-		rospy.loginfo("Shutting down subscriber!")
+		rospy.loginfo("Shutting down synchronizer!")
 		sp.shutdown()
